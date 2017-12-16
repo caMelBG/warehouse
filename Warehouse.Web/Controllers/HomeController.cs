f3cc5e6f9cc51e0c9123f84463b41b2a31c196a1
camelbg <tihomir.ivanov1564@gmail.com>
2017-12-17 01:16:20 +0200

    InitialCommit

---
+++ Warehouse.Web/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Warehouse.Web.Data;
+using Warehouse.Web.Models;
+
+namespace Warehouse.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationDbContext _db;
+
+        public HomeController(ApplicationDbContext db)
+        {
+            this._db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var email = this._db.Users.First()?.Email;
+            return View("Index", email);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
